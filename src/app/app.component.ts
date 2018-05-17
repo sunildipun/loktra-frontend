a66c1189e326ccaa01eb743dcd44294a59ceec6c
sunil2171 <sunil.6318ie@gmail.com>
2018-05-17 12:13:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component , Inject} from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+
+import {MatSnackBar} from '@angular/material';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  donate: FormGroup;
+  count :number = 0;
+  donateNumber:number = 0;
+  remaining: number = 0;
+  money: number = 0;
+  moneyValue : number = 0;
+  showText: boolean;
+
+  constructor(private fb: FormBuilder,
+              public snackBar: MatSnackBar, 
+              public dialog: MatDialog,         
+            ){}
+
+  ngOnInit()
+  {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.donate = this.fb.group({
+      money:    ['', Validators.required ],
+    })
+  }
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(DialogDialog, {
+      width: '250px',
+      
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+  
+  number(f){
+    console.log(f.value);
+    this.donateNumber = Number(f.value.money);
+  }
+
+
+  donateMoney(f)
+  { console.log(this.moneyValue);
+    this.count++;
+    if(this.moneyValue <= 100){
+      console.log('Money Donated Value', f.value.money);
+      this.money = this.money + Number(f.value.money);
+      console.log('Total Money', this.money);
+      this.remaining = 1000 - this.money;
+      this.moneyValue = this.money/10;
+    }
+    else{
+      this.snackBar.open('Thank You !','But we have reached required fund', {
+        duration: 2000,
+      });
+    }
+    
+  }
+
+}
+
+
+@Component({
+  selector: 'dialog',
+  templateUrl: './dialog.html',
+})
+export class DialogDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import {MatCardModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule, MatProgressBarModule, MatSnackBarModule, MatDialogModule} from '@angular/material';
+
+import { AppComponent, DialogDialog } from './app.component';
+
+// import { ShareButtonModule } from '@ngx-share/button';
+import { ShareButtonModule } from 'ngx-sharebuttons';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogDialog
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatDialogModule,
+
+    HttpClientModule,       // for share counts
+    HttpClientJsonpModule,  // for linkedin and tumblr share counts
+    ShareButtonModule.forRoot()
+
   ],
   providers: [],
+  entryComponents: [AppComponent, DialogDialog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
